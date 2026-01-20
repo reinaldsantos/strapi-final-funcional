37650a0d68234feadde976cd1f196dde308ed641
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-20 09:50:10 +0000

    Strapi 4.17.0 funcional para deploy

---
+++ config/database.js
@@ -0,0 +1,19 @@
+﻿const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const config = parse(env("DATABASE_URL"));
+  
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: { rejectUnauthorized: false },
+      },
+    },
+  };
+};
