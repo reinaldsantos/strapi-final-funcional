19653e675a3eecb0738fe80e7270f81a40026944
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-20 11:32:08 +0000

    feat: configure CORS for frontend access

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,36 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://strapi-final-funcional.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://strapi-final-funcional.onrender.com'],
+          'script-src': ["'self'", "'unsafe-inline'"],
+          'style-src': ["'self'", "'unsafe-inline'"],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://localhost:8080',
+        'https://strapi-final-funcional.onrender.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
